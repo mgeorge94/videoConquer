bf502a8a1b9f0eadee42ce54c7f56bcc12a7a481
George Murgoci <george.murgoci94@gmail.com>
2023-02-04 17:00:50 +0200

    added home and about components /refactoring/ routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import {environment} from 'src/environments/environment';
 import {AngularFireModule} from '@angular/fire/compat';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
-	declarations: [AppComponent, NavComponent],
+	declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
