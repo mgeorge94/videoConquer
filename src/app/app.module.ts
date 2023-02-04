69ba1472e2a298387d05e9ac8fd1c3cc38d9689b
George Murgoci <george.murgoci94@gmail.com>
2023-02-04 18:16:01 +0200

    created manage page and added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {UserModule} from './user/user.module';
-import {NavComponent} from './nav/nav.component';
-import {environment} from 'src/environments/environment';
-import {AngularFireModule} from '@angular/fire/compat';
-import {AngularFireAuthModule} from '@angular/fire/compat/auth';
-import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { NavComponent } from './nav/nav.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { VideoModule } from './video/video.module';
 @NgModule({
 	declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent],
 	imports: [
@@ -20,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 		AngularFireModule.initializeApp(environment.firebase),
 		AngularFireAuthModule,
 		AngularFirestoreModule,
+		VideoModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
