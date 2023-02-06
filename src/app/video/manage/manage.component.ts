fa02c880dd3ec0e669911891d89d6e3a586dbff2
George Murgoci <george.murgoci94@gmail.com>
2023-02-06 10:59:08 +0200

    redirect user from atuh only page when logout

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-manage',
-  templateUrl: './manage.component.html',
-  styleUrls: ['./manage.component.css']
+	selector: 'app-manage',
+	templateUrl: './manage.component.html',
+	styleUrls: ['./manage.component.css'],
 })
-export class ManageComponent {
-
-}
+export class ManageComponent {}
