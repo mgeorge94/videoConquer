309f630c6826d12c0a4ecfdfe1ca6da440b600fc
George Murgoci <george.murgoci94@gmail.com>
2023-02-08 13:47:08 +0200

    fixed hover class when dropping file onto drop box

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-upload',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+	selector: 'app-upload',
+	templateUrl: './upload.component.html',
+	styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent {
-
+	isDragover = false;
+	storeFile(e: Event) {
+		console.log('e', e);
+		this.isDragover = false;
+	}
 }
