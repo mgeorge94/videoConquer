3778137c2f2350005a26b40e17ff424669171f5d
George Murgoci <george.murgoci94@gmail.com>
2023-02-08 13:21:56 +0200

    added authGuard for accessing an authonly page via url

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 const routes: Routes = [
 	{
 		path: 'manage',
 		component: ManageComponent,
 		data: {
 			authOnly: true,
+			authGuardPipe: () => redirectUnauthorizedTo('/'),
 		},
+		canActivate: [AngularFireAuthGuard],
 	},
 	{
 		path: 'upload',
 		component: UploadComponent,
 		data: {
 			authOnly: true,
+			authGuardPipe: () => redirectUnauthorizedTo('/'),
 		},
+		canActivate: [AngularFireAuthGuard],
 	},
 	{
 		path: 'manage-clips',
