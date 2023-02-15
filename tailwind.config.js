2f9029e1f412dc46e92f762ed89ee220acf7ea5a
George Murgoci <george.murgoci94@gmail.com>
2023-02-15 17:31:03 +0200

    added safeloading for tailwind to create some classes dinamically

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './src/**/*.{html,ts}',
   ],
-  safelist:["bg-blue-400", "bg-green-400", "bg-red-400"],
+  safelist:["bg-blue-400", "bg-green-400", "bg-red-400", 'text-indigo-100', 'text-indigo-200', 'text-indigo-300', 'text-indigo-400','text-indigo-500','text-indigo-600','text-indigo-700','text-indigo-800','text-indigo-900',],
   theme: {
     extend: {},
   },
